7b7efa793ec2ceff13fff84b3f577dc0cdca7ce4
DenysShpak0116 <shpak.denys.oleks@gmail.com>
2026-08-09 19:42:48 +0000

    feat: implement udp parser

---
+++ network_parser/src/udp.rs
@@ -0,0 +1,43 @@
+use crate::error::ParseError;
+
+const MIN_UDP_HEADER_SIZE: usize = 8;
+
+pub struct UdpDatagram<'a> {
+    pub source_port: u16,
+    pub destination_port: u16,
+    pub length: u16,
+    pub checksum: u16,
+    pub payload: &'a [u8],
+}
+
+impl<'a> UdpDatagram<'a> {
+    pub fn new(datagram: &'a [u8]) -> Result<Self, ParseError> {
+        let (header, rest_payload) = datagram
+            .split_first_chunk::<MIN_UDP_HEADER_SIZE>()
+            .ok_or(ParseError::PacketTooShort)?;
+
+        let source_port = u16::from_be_bytes([header[0], header[1]]);
+        let destination_port = u16::from_be_bytes([header[2], header[3]]);
+        let length = u16::from_be_bytes([header[4], header[5]]);
+
+        let length_usize = length as usize;
+        if length_usize < MIN_UDP_HEADER_SIZE {
+            return Err(ParseError::InvalidUdpLength);
+        }
+
+        let checksum = u16::from_be_bytes([header[6], header[7]]);
+
+        let payload_length = length_usize - MIN_UDP_HEADER_SIZE;
+        let payload = rest_payload
+            .get(..payload_length)
+            .ok_or(ParseError::InvalidUdpLength)?;
+
+        Ok(UdpDatagram {
+            source_port,
+            destination_port,
+            length,
+            checksum,
+            payload,
+        })
+    }
+}
