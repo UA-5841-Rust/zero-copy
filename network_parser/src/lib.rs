c2619a4b3fc3f58f9f34081cb74fce3831ad5c9d
DenysShpak0116 <shpak.denys.oleks@gmail.com>
2026-08-09 16:26:21 +0000

    feat: use ethernet parsing function in lib.rs

--- network_parser/src/lib.rs
+++ network_parser/src/lib.rs
@@ -1,17 +1,31 @@
 mod error;
 mod ethernet;
 
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use error::ParseError;
+use ethernet::EthernetHeader;
+
+const ETHERNET_HEADER_SIZE: usize = 14;
+const MIN_IPV4_HEADER_SIZE: usize = 20;
+const MIN_UDP_DATAGRAM_SIZE: usize = 8;
+const MIN_DATA_SIZE: usize = ETHERNET_HEADER_SIZE + MIN_IPV4_HEADER_SIZE + MIN_UDP_DATAGRAM_SIZE;
+
+pub struct Packet<'a> {
+    pub ethernet: EthernetHeader<'a>,
+    // pub ipv4: Option<Ipv4Header>,
+    // pub udp: Option<UdpPacket<'a>>,
+}
+
+pub fn parse_packet(data: &[u8]) -> Result<Packet<'_>, ParseError> {
+    if data.len() < MIN_DATA_SIZE {
+        return Err(ParseError::PacketTooShort);
+    }
+
+    let ethernet_header = EthernetHeader::new(data)?;
+
+    Ok(Packet {
+        ethernet: ethernet_header,
+    })
 }
 
 #[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+mod tests {}
