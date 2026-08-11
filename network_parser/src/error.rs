76bcb46714f9dd6a18a0f3f98d80aa5fdc5151d6
annatyshchenko33 <annati4nko@gmail.com>
2026-08-11 23:12:06 +0300

    feat(network_parser): add error types

---
+++ network_parser/src/error.rs
@@ -0,0 +1,9 @@
+#[derive(Debug, PartialEq)]
+pub enum ParseError {
+    PacketTooShort,
+    InvalidEtherType,
+    InvalidIpv4Version,
+    InvalidIpv4HeaderLength,
+    InvalidIpv4TotalLength,
+    InvalidUdpLength,
+}
